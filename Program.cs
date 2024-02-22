207da937e0e828f9f7fed06c17929fe5f820a19e
csabadomjan <csaba.domjan@stud.uni-corvinus.hu>
2024-02-22 11:22:25 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace Szorzotabla
+{
+
+    public partial class Form1: Form
+
+        public Form1()
+    {
+        InitializeComponent()
+
+    }
+}
